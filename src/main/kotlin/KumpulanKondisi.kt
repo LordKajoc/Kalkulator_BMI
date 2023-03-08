057578b6b69cc1fcde33b3179f85629627d7971b
LordKajoc <rizkinuriman@upi.edu>
2023-03-08 21:17:11 +0700

    PErbaikan 1

--- src/main/kotlin/KumpulanKondisi.kt
+++ src/main/kotlin/KumpulanKondisi.kt
@@ -1,12 +1,10 @@
 abstract class Kondisi{
-    fun inputuser(){
+    fun hitung(){
     print("Masukan Berat Badan (kg): ")
     val beratbadan: Double = readln().toDouble()
     print("Masukan Tinggi Badan (cm): ")
     var tinggibadan: Double = readln().toDouble()
     tinggibadan = tinggibadan / 100.0
+        var BMI: Double = (beratbadan / (tinggibadan * tinggibadan))
 }
-    class hitung: Kondisi() {
-
-    }
 }
