4bce6fec20350c2ee0260d0a32e350c755b5f04d
LordKajoc <rizkinuriman@upi.edu>
2023-03-08 21:09:30 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import javax.net.ssl.HostnameVerifier
+
+fun main(args: Array<String>) {
+}
+
+fun calculateBMI() {
+    /*
+    var BMI: Double = (beratbadan / (tinggibadan * tinggibadan))
+    if (BMI < 17.0) {
+        println("Hasil BMI: Kurus")
+    } else if (BMI in 17.0..18.5) {
+        println("Hasil BMI: Kurus")
+    } else if (BMI in 18.5..25.0) {
+        println("Hasil BMI: Normal")
+    } else if (BMI in 25.0..27.0) {
+        println("Hasil BMI: Gemuk")
+    } else if (BMI > 27) {
+        println("Hasil BMI: Gemuk")
+    }*/
+}
